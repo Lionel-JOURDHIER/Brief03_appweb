3d4620dc9dde19467298851c815370d37ba22c0f
Lionel JOURDHIER <lio.jourdhier@gmail.com>
2025-12-09 17:23:44 +0100

    Modification des pages inserer et rechercher pour affichage des sentiments des citations

--- frontend/pages/0_inserer.py
+++ frontend/pages/0_inserer.py
@@ -11,11 +11,14 @@ logger.remove()
 logger.add("frontend/logs/streamlit.log")
 
 load_dotenv()
-API_PORT = int(os.getenv('FASTAPI_PORT'))
-API_URL = os.getenv('API_ROOT_URL')
-API_ROOT_URL = f"http://{API_URL}:{API_PORT}"
+IA_API_PORT = int(os.getenv('IA_API_PORT'))
+FASTAPI_PORT = int(os.getenv('FASTAPI_PORT'))
+API_ROOT_URL = os.getenv('API_ROOT_URL')
+API_IA_ROOT_URL = f"http://{API_ROOT_URL}:{IA_API_PORT}"
+API_DATA_ROOT_URL = f"http://{API_ROOT_URL}:{FASTAPI_PORT}"
 
-API_URL = API_ROOT_URL + "/insert"
+API_DATA_URL = API_DATA_ROOT_URL + "/insert/"
+API_IA_URL = API_IA_ROOT_URL + "/sentiment/"
 
 st.title("Inserer une nouvelle citation")
 
@@ -29,22 +32,27 @@ with st.form("insert form"):
         try:
             # 1. --- Requetes POST sur la route '/insert/' ---
             logger.info(f"Requete POST sur la route '/insert/'")
-            reponse = requests.post(API_URL, json=data)
-            
+            reponse = requests.post(API_DATA_URL, json=data)
+            logger.info(f"Requete POST sur la route IA : '/sentiment/'")
+            sentiment = requests.post(API_IA_URL, json=data)
             if reponse.status_code == 200:
                 # 2. --- Si il y a un resultat l'afficher ---
                 result = reponse.json()
+                result_sentiment = sentiment.json()
                 st.success(f"Citation ajoutée ! ID: {result['id']} ")
                 logger.info(f"Citation ajoutée ! ID: {result['id']} ")
                 st.json(result)
+                st.success(f"Votre citation à les sentiments suivants : ")
+                st.json(result_sentiment)
+                logger.info(f"Reponse des sentiments {result_sentiment}")
                 st.balloons()
             else : 
                 st.error(f"L'API a répondu avec une erreur : {reponse.status_code}")
                 logger.error(f"L'API a répondu avec une erreur : {reponse.status_code}")
 
         except requests.exceptions.ConnectionError : 
-            st.error(f"ERREUR : Impossible de se connecter à l'API à {API_URL}")
-            logger.error(f"ERREUR : Impossible de se connecter à l'API à {API_URL}")
+            st.error(f"ERREUR : Impossible de se connecter à l'API à {API_DATA_URL}")
+            logger.error(f"ERREUR : Impossible de se connecter à l'API à {API_DATA_URL}")
             st.warning("Veuillez vous assurer que le serveur est bien lancé en arrière plans")
             logger.error("Veuillez vous assurer que le serveur est bien lancé en arrière plans")
 
--- frontend/pages/2_Rechercher.py
+++ frontend/pages/2_Rechercher.py
@@ -12,10 +12,13 @@ logger.add("frontend/logs/streamlit.log")
 
 load_dotenv()
 
-API_PORT = int(os.getenv('FASTAPI_PORT'))
-API_URL = os.getenv('API_ROOT_URL')
-API_ROOT_URL = f"http://{API_URL}:{API_PORT}"
+IA_API_PORT = int(os.getenv('IA_API_PORT'))
+FASTAPI_PORT = int(os.getenv('FASTAPI_PORT'))
+API_ROOT_URL = os.getenv('API_ROOT_URL')
+API_IA_ROOT_URL = f"http://{API_ROOT_URL}:{IA_API_PORT}"
+API_DATA_ROOT_URL = f"http://{API_ROOT_URL}:{FASTAPI_PORT}"
 
+API_IA_URL = API_IA_ROOT_URL + "/sentiment/"
 
 st.title("Lire une citation")
 
@@ -25,18 +28,25 @@ mode = st.radio("Choisissez le mode de recherche:",
 if mode == "Aléatoire":
     st.subheader("Citation Aléatoire")
     # afficher une citation aléatoire
-    API_URL =  API_ROOT_URL + "/read/random/"
+    API_DATA_URL =  API_DATA_ROOT_URL + "/read/random/"
     if st.button("obtenir une citation aléatoire:"):
         try : 
-            response = requests.get(API_URL)
+            response = requests.get(API_DATA_URL)
             logger.info(f"Requete GET sur la route '/read/random/'")
+            
             if response.status_code == 200:
                 result = response.json()
+                data = {'text': result.get('text', 'text non trouvé')}
+                logger.info(f"Requete POST sur la route IA : '/sentiment/'")
+                sentiment = requests.post(API_IA_URL, json=data)
+                result_sentiment = sentiment.json()
 
                 if result:
                     st.success(f"Citation avec ID {result.get('id', 'N/A')}")
                     logger.info(f"Citation avec ID {result.get('id', 'N/A')}")
                     st.info(result.get('text', 'text non trouvé'))
+                    st.json(result_sentiment)
+                    logger.info(f"Reponse des sentiments {result_sentiment}")
                     st.balloons()
                 else:
                     st.warning("Aucune citation disponible dans la DB")
@@ -47,15 +57,15 @@ if mode == "Aléatoire":
 
 
         except requests.exceptions.ConnectionError : 
-            st.error(f"ERREUR : Impossible de se connecter à l'API à {API_URL}")
-            logger.error(f"ERREUR : Impossible de se connecter à l'API à {API_URL}")
+            st.error(f"ERREUR : Impossible de se connecter à l'API à {API_DATA_URL}")
+            logger.error(f"ERREUR : Impossible de se connecter à l'API à {API_DATA_URL}")
             st.warning("Veuillez vous assurer que le serveur est bien lancé en arrière plans")
             logger.error("Veuillez vous assurer que le serveur est bien lancé en arrière plans")
 
 else:
     # afficher une citation par ID
     st.subheader("Citation par ID")
-    API_URL =  API_ROOT_URL + "/read/"
+    API_DATA_URL =  API_DATA_ROOT_URL + "/read/"
     # selectionne l'ID
     # un formulaire
     with st.form("search_by_id"):
@@ -67,16 +77,22 @@ else:
     if submitted:
         # appel la route /read/id
         try : 
-            response = requests.get( API_URL + str(quote_id) )
+            response = requests.get(API_DATA_URL + str(quote_id) )
             logger.info(f"Requete GET sur la route '/read/{quote_id}'")
         # le reste est pareil
             if response.status_code == 200:
                 result = response.json()
+                data = {'text': result.get('text', 'text non trouvé')}
+                logger.info(f"Requete POST sur la route IA : '/sentiment/'")
+                sentiment = requests.post(API_IA_URL, json=data)
+                result_sentiment = sentiment.json()
 
                 if result:
                     st.success(f"Citation avec ID {quote_id}")
                     st.info(result.get('text', 'text non trouvé'))
                     logger.info(f"Citation avec ID {quote_id} : {result.get('text', 'text non trouvé')}")
+                    st.json(result_sentiment)
+                    logger.info(f"Reponse des sentiments {result_sentiment}")
                     st.balloons()
                 else:
                     st.warning(f"La citation {quote_id} n'est pas disponible dans la DB")
@@ -92,7 +108,7 @@ else:
 
 
         except requests.exceptions.ConnectionError:
-            st.error(f"ERREUR : Impossible de se connecter à l'API à {API_URL}")
+            st.error(f"ERREUR : Impossible de se connecter à l'API à {API_DATA_URL}")
             st.warning("Veuillez vous assurer que le serveur Uvicorn est bien lancé en arrière-plan.")
 
 
