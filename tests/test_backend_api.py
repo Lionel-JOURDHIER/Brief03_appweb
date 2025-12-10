28262f61c8280f7d97984372c7b3c73bfc285f99
Lionel JOURDHIER <lio.jourdhier@gmail.com>
2025-12-10 16:08:38 +0100

    test API IA et requete POST

--- tests/test_backend_api.py
+++ tests/test_backend_api.py
@@ -25,3 +25,28 @@ def test_root():
         logger.error("Requete GET sur la route '/' : echec")
     assert test
 
+client2 = TestClient(app_ia)
+
+def test_ia():
+    test_sentiment_text = "congratulation"
+    data = {'text': test_sentiment_text}
+    response = client2.post("/sentiment/", json=data)
+    test1 = response.status_code == 200
+    if  test1 : 
+        logger.info("Requete POST sur la route '/sentiment/' : reussite")
+    else : 
+        logger.error("Requete POST sur la route '/sentiment/' : echec")
+    assert test1
+    data_response = response.json()
+
+    test2 = data_response == {
+        "neg":0,
+        "neu":0,
+        "pos":1,
+        "compound":0.5994
+        }
+    if  test2 : 
+        logger.info("Verification des sentiments : reussite")
+    else : 
+        logger.error("Verification des sentiments : echec")
+    assert test2
